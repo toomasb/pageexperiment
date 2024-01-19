8f56fc948fba6eb6d63bb4b5e1a31af694f8854a
Thomas <boogaerts.thomas@gmail.com>
2024-01-19 13:47:01 +0100

    v0.1.17

--- components/PageExperimentMiddleware.jsx
+++ components/PageExperimentMiddleware.jsx
@@ -1,10 +1,12 @@
 import { NextResponse } from 'next/server';
-import { v4 as uuidv4 } from 'uuid';
-
 
 export function PageExperimentMiddleware(req, experiments) {
 
-    console.log(experiments)
+    function uuidv4() {
+        return "10000000-1000-4000-8000-100000000000".replace(/[018]/g, c =>
+          (c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16)
+        );
+    }
 
     const response = NextResponse.next();
 
--- dist/PageExperimentMiddleware.js
+++ dist/PageExperimentMiddleware.js
@@ -1,8 +1,11 @@
 import { NextResponse } from 'next/server';
-import { v4 as uuidv4 } from 'uuid';
 export function PageExperimentMiddleware(req, experiments) {
   var _req$cookies$get;
-  console.log(experiments);
+  function uuidv4() {
+    return "10000000-1000-4000-8000-100000000000".replace(/[018]/g, function (c) {
+      return (c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16);
+    });
+  }
   var response = NextResponse.next();
   var visitor_guid = (_req$cookies$get = req.cookies.get('pageExperiment_visitor_guid')) === null || _req$cookies$get === void 0 ? void 0 : _req$cookies$get.value;
   if (!visitor_guid) {
